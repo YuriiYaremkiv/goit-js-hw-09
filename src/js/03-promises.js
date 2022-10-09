4f23928763338d676851bf28beef98dd9378218c
Yuriy Yaremkiv <y.yaremkiv@gmail.com>
2022-10-09 21:17:16 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+	form: document.querySelector('.form')
+}
+
+refs.form.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	const { delay, step, amount } = event.target.elements;
+  console.log(event.target.elements);
+
+	let nextSteps = Number(delay.value);
+
+	for (let i = 1; i <= Number(amount.value); i += 1) {
+
+		createPromise(i, nextSteps)
+			.then(({ position, delay }) => {
+				Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+			})
+			.catch(({ position, delay }) => {
+				Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+			});
+
+		nextSteps += Number(step.value);
+	}
+})
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+	const shouldResolve = Math.random() > 0.3;
+
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if (shouldResolve) {
+				resolve({ position, delay })
+			} else {
+				reject({ position, delay })
+			}
+		}, delay)
+	})
 }
